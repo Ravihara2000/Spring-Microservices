e961f28aa17227cc8bf79cd3886b943cf4659d7a
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2024-05-19 17:49:45 +0530

    Rest template communication
    get the details for employee and department using rest template

---
+++ Section 2 Communication/EmployeeManagementMicroservices -communication/employee-service/src/main/java/com/SpringbootLearning/employeeservice/EmployeeServiceApplication.java
@@ -0,0 +1,20 @@
+package com.SpringbootLearning.employeeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class EmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+}
