9cf1dc310d376565605a78aa98ca5a6de7e8d7ae
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2024-05-22 09:27:46 +0530

    take a copy of service registry to implement Api Gateway

---
+++ Section 4 Api Gateway/EmployeeManagementMicroservices -Api Gateway/department-service/src/main/java/com/SpringbootLearning/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,51 @@
+package com.SpringbootLearning.departmentservice.service.impl;
+
+import com.SpringbootLearning.departmentservice.dto.DepartmentDto;
+import com.SpringbootLearning.departmentservice.entity.Department;
+import com.SpringbootLearning.departmentservice.repository.DepartmentRepository;
+import com.SpringbootLearning.departmentservice.service.DepartmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+/*    public DepartmentServiceImpl(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }*/
+
+    @Override
+    public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
+        Department department = new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentName(),
+                departmentDto.getDepartmentDescription(),
+                departmentDto.getDepartmentCode()
+        );
+
+        Department savedDepartment = departmentRepository.save(department);
+
+        DepartmentDto savedDepartmentDto = new DepartmentDto(
+                savedDepartment.getId(),
+                savedDepartment.getDepartmentName(),
+                savedDepartment.getDepartmentDescription(),
+                savedDepartment.getDepartmentCode()
+        );
+        return savedDepartmentDto;
+    }
+
+    @Override
+    public DepartmentDto getDepartmentByCode(String code) {
+        Department department = departmentRepository.findByDepartmentCode(code);
+        DepartmentDto departmentDto = new DepartmentDto(
+                department.getId(),
+                department.getDepartmentName(),
+                department.getDepartmentDescription(),
+                department.getDepartmentCode()
+        );
+        return departmentDto;
+    }
+}
